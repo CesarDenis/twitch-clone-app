83dc3499e731cd2a7f6b516a007596a496cfb778
Cesar Denis <cesar@cesardenis.com>
2020-08-30 17:02:29 -0300

    build: enable new logbox

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+// add the new LogBox
+require('react-native').unstable_enableLogBox();
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
